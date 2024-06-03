a0b083895508bb2f91bc1a7d0b4acb4d22366714
MarouaTamoussit <marouatamoussit008@gmail.com>
2024-06-03 21:49:11 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from joblib import load
+
+app = Flask(__name__)
+
+# Charger le modèle
+model = load('heartAttackrisque.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    # Effectuer la prédiction
+    prediction = model.predict([data])[0]  # Suppose que la prédiction est une liste de résultats (à adapter selon votre modèle)
+    return jsonify({'prediction': prediction})
+
+if __name__ == '__main__':
+    app.run(debug=True)
