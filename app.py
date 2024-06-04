fff967f8f46feb1f5572e938bea64939fab95fdd
MarouaTamoussit <148471422+MarouaTamoussit@users.noreply.github.com>
2024-06-04 02:51:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def predict():
     prediction = model.predict([data])[0]  # Suppose que la prédiction est une liste de résultats (à adapter selon votre modèle)
     return jsonify({'prediction': prediction})
 
-
- if __name__ == '__main__':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))  # Utilisez le port défini par la variable d'environnement PORT ou 10000 par défaut
-    app.run(debug=True, port=port)  
+    app.run(debug=True, port=port)
